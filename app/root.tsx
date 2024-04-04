c17dc4477b3cc89c9c76545e1b3165a04a63e35c
KevinDev01 <kevinfelixm.1@gmail.com>
2024-04-03 17:31:41 -0700

    update stylesheet

--- app/root.tsx
+++ app/root.tsx
@@ -5,12 +5,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
-import stylesheet from "~/styles/tailwind.css?url";
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
-];
+import "~/styles/tailwind.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
