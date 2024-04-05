b4309049bbafaf576c8ca71255eb8041b3e14db7
KevinDev01 <kevinfelixm.1@gmail.com>
2024-04-04 19:15:12 -0700

    Update admin navbar

--- app/routes/_admin.tsx
+++ app/routes/_admin.tsx
@@ -11,24 +11,24 @@ import Authenticator from "~/services/auth.server";
 export default function AdminLayout() {
   return (
     <div className="overflow-hidden h-screen py-2">
-      <nav className="w-full h-16 px-5 flex items-center justify-between rounded-md border shadow-sm">
+      <nav className="w-full h-16 px-5 bg-indigo-400 flex items-center justify-between rounded-md shadow-sm">
         <ul>
           <h1 className="text-lg">Panel Administrador</h1>
         </ul>
         <ul className="flex gap-2">
           <Link
             to={"/panel/tracking"}
-            className="w-32 h-12 bg-stone-100 flex items-center justify-center rounded-md hover:bg-stone-200 transition ease-out">
+            className="w-32 h-12 bg-white flex items-center justify-center rounded-md hover:bg-indigo-100 transition ease-out">
             Ver pedidos
           </Link>
           <Link
             to={"/panel/config"}
-            className="w-fit h-12 px-5 bg-stone-100 flex items-center justify-center rounded-md hover:bg-stone-200 transition ease-out">
+            className="w-fit h-12 px-5 bg-white flex items-center justify-center rounded-md hover:bg-indigo-100 transition ease-out">
             Editar publicaciones
           </Link>
           <Link
             to={"/panel/create"}
-            className="w-32 h-12 bg-stone-100 flex items-center justify-center rounded-md hover:bg-stone-200 transition ease-out">
+            className="w-32 h-12 bg-white flex items-center justify-center rounded-md hover:bg-indigo-100 transition ease-out">
             Crear producto
           </Link>
         </ul>
